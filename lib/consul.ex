16c360dce7279a473816acbb31b1cbe00db83956
William Huba <william@trywildcard.com>
2015-01-28 12:16:38 -0500

    Consul node url helper

--- lib/consul.ex
+++ lib/consul.ex
@@ -169,11 +169,14 @@ defmodule Consul do
 
 
   defmodule Nodes do
+    defp node_url(node), do: Consul.base_uri <> "/catalog/node/" <> node
+
+
     @doc """
     Looks up the IP address of a node using consul.
     """
-    def address(node), do: "#{Consul.base_uri}/catalog/node/#{node}" |> _address
-    def address(node, datacenter), do: "#{Consul.server}/v1/catalog/node/#{node}?dc=#{datacenter}" |> _address
+    def address(node), do: node_url(node) |> _address
+    def address(node, datacenter), do: node_url(node) <> "?dc=#{datacenter}" |> _address
     defp _address(url) do
       url |> Consul.get_json |> Dict.get("Node") |> Dict.get("Address")
     end
