a4aaee378c8bb2794130f8c57b2bc49c1f9b8fd1
William Huba <william@trywildcard.com>
2015-01-28 12:17:00 -0500

    Module for querying nodes responsible for a given service in consul.

--- lib/consul.ex
+++ lib/consul.ex
@@ -180,5 +180,41 @@ defmodule Consul do
     defp _address(url) do
       url |> Consul.get_json |> Dict.get("Node") |> Dict.get("Address")
     end
+
+    def info(node) do
+      node_url(node) |> Consul.get_json
+    end
+  end
+
+
+  defmodule Services do
+    defp service_url(service), do: Consul.base_uri <> "/catalog/service/" <> service
+
+    def info(service), do: service_url(service) |> Consul.get_json
+
+    @doc """
+    Looks up all nodes running a particular service.
+    """
+    def nodes(service) do
+      service_url(service)
+          |> Consul.get_json
+          |> Enum.map(&(&1["Node"]))
+          |> Enum.uniq
+    end
+
+    @doc """
+    Looks up healthy nodes running a particular service
+    """
+    def nodes(service, :passing) do
+      Consul.base_uri <> "/health/service/" <> service
+          |> Consul.get_json
+          |> Enum.filter(&health_filter/1)
+          |> Enum.map(&(&1["Node"]["Node"]))
+          |> Enum.uniq
+    end
+
+    defp health_filter(service, status \\ "passing") do
+      Enum.all?(service["Checks"], &(&1["Status"] == status))
+    end
   end
 end
