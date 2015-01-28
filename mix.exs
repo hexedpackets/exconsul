a0ca41cd21e136aa41a94a950a33dbf20e7d8e23
William Huba <william@trywildcard.com>
2015-01-28 15:19:52 -0500

    Remove umbrella references.

--- mix.exs
+++ mix.exs
@@ -4,8 +4,6 @@ defmodule Consul.Mixfile do
   def project do
     [app: :consul,
      version: "0.0.2",
-     deps_path: "../../deps",
-     lockfile: "../../mix.lock",
      elixir: "~> 1.0",
      deps: deps]
   end
