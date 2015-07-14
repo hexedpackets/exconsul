dd92893e7130a166d7c2b673cedde24e8dc56dca
William Huba <william@trywildcard.com>
2015-07-14 12:35:59 -0400

    Updated package metadata.

--- mix.exs
+++ mix.exs
@@ -1,11 +1,21 @@
 defmodule Consul.Mixfile do
   use Mix.Project
 
+  @version File.read!("VERSION") |> String.strip
+
   def project do
     [app: :consul,
-     version: "0.1.1",
+     version: @version,
      elixir: "~> 1.0",
-     deps: deps]
+     deps: deps,
+     name: "Consul",
+     docs: [readme: "README.md", main: "README",
+            source_ref: "v#{@version}",
+            source_url: "https://github.com/hexedpackets/exconsul"],
+
+     # Hex
+     description: description,
+     package: package]
   end
 
   def application do
@@ -17,5 +27,18 @@ defmodule Consul.Mixfile do
       {:poison, "~> 1.2"},
       {:httpoison, "~> 0.5"}
     ]
+  end
+
+  defp description do
+    """
+    A wrapper around the Consul API with some opinionated helper functionality.
+    """
+  end
+
+  defp package do
+    [contributors: ["William Huba"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/hexedpackets/exconsul"},
+     files: ~w(mix.exs README.md LICENSE lib VERSION)]
   end
 end
